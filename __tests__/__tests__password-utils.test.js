5bb4ab994e9e83a5266284e8353673178f8ebb5f
Kyrillos Abdalla <kyrillosabdalla1@gmail.com>
2026-04-15 15:22:59 +0200

    added initial Jest tests

---
+++ __tests__/__tests__password-utils.test.js
@@ -0,0 +1,19 @@
+const {
+    generateStrongPassword,
+    isStrongPassword,
+} = require("../index");
+
+describe("Password Utilities", () => {
+    test("isStrongPassword should return false for weak password", () => {
+        expect(isStrongPassword("Hello World")).toBe(false);
+    });
+
+    test("isStrongPassword should return true for strong password", () => {
+        expect(isStrongPassword("pC%mD8TpCKn2")).toBe(true);
+    });
+
+    test("generateStrongPassword should return password with requested length", () => {
+        const password = generateStrongPassword(12);
+        expect(password).toHaveLength(12);
+    });
+});
